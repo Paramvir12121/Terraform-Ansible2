32cbcb61c067ea6f71c60337a9aad109b483cae4
Paramvir Singh <paramvir12121@gmail.com>
2023-04-19 19:44:15 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-#terraform {
-# backend "azurerm" {
-#    resource_group_name  = "9198-assignment1-RG"
-#    storage_account_name = "storageaccountrutul9198"
-#    container_name       = "tfstatefiles"
-#    key                  = "E2UGmnBVUHP9u+QdC9puwwfpuJmbThyKB/E21uOgNajSDR1T9lnI6wZ7qBPSfS+1f+1jN7UZuLsB+AStj8GOtg=="
-#  }
-#}
+terraform {
+backend "azurerm" {
+   resource_group_name  = "9079-Assignment2-RG"
+   storage_account_name = "storageaccountparamvir"
+   container_name       = "tfstatefiles"
+   key                  = "Ou3h+H2q8H4CwJUO1n74pS1l6bFsHe9+KIpoAifPr9/rAEkyYO+pF8Nszg0SLoxPqgwqlgeRJVj6+AStiezYeQ=="
+ }
+}
